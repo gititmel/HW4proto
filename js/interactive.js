fe6e5b1de7cb28d4ab80f5f69735ca2792b9339a
gititmel <melandrea@yahoo.com>
2016-05-16 20:54:46 -0400

    added a popup!

---
+++ js/interactive.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+	console.log("lightbox initialized");
+
+	$("#lbTrigger").click( function(){
+		// $("#lightbox").fadeIn( 2000 );
+		$("#lightbox").show();
+		console.log("cliked the button");
+	})
+
+	// $("#closeBtn").click( function(){
+	// 	$("#lightbox").hide();
+	// })
+
+
+});
